f59821a1c76d312998a419dc45f1ead23fcde1ad
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2021-07-14 21:24:39 -0300

    Create short_url.py

---
+++ short_url.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# pip install --user pyshorteners
+
+import os
+import pyshorteners
+
+os.system('clear')
+url = input('Cole a url aqui:\n\n')
+s = pyshorteners.Shortener()
+shortUrl = s.tinyurl.short(url)
+print(f'Url encurtada: {shortUrl}')
